f27b04fff233334ec704d94318eefe3814df6877
kexiaojun <931714827@qq.com>
2016-11-23 22:34:41 +0800

    sprint one

---
+++ OrderSys/src/com/example/ordersys/MenuActivity.java
@@ -0,0 +1,14 @@
+package com.example.ordersys;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MenuActivity extends Activity {
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+	// TODO Auto-generated method stub
+	super.onCreate(savedInstanceState);
+	setContentView(R.layout.menu_layout);
+}
+
+}
