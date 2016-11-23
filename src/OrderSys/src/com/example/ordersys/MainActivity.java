eb853815a5b4a09d88de1e7d6cb66607f776cb15
kexiaojun <931714827@qq.com>
2016-11-23 23:43:18 +0800

    sprint one

---
+++ src/OrderSys/src/com/example/ordersys/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.ordersys;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+	Button login;
+  
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		login=(Button)findViewById(R.id.login);
+		login.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				Intent intent=new Intent();
+        		intent.setClass(MainActivity.this,MenuActivity.class);
+        		startActivity(intent);
+        		finish();
+			}
+		});
+		
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
